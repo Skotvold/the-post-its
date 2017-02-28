0405e06211604008e44d8eaa29928b3d7dce9526
Eskil Skotvold <eskilas@stud.ntnu.no>
2017-02-28 03:00:01 +0100

    Controller scripts added, need to test with proper vr gear

---
+++ vr-chemist/Assets/Scripts/ControllerGrabObject.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrabObject : MonoBehaviour {
+
+	private SteamVR_TrackedObject mTrackedObj;
+    private GameObject collidingObject;
+    private GameObject objectInHand;
+
+    private SteamVR_Controller.Device mController
+    {
+        get { return SteamVR_Controller.Input((int)mTrackedObj.index); }
+    }
+
+    void Awake()
+    {
+        mTrackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void SetCollidingObject(Collider col)
+    {
+       
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+     
+        collidingObject = col.gameObject;
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+
+
+
+    private void GrabObject()
+    {
+        objectInHand = collidingObject;
+        collidingObject = null;
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    private void ReleaseObject()
+    {
+        if (GetComponent<FixedJoint>())
+        {
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+
+            objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+        }
+
+        objectInHand = null;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ vr-chemist/Assets/Scripts/ViveControllerInput.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveControllerInput : MonoBehaviour
+{
+    private SteamVR_TrackedObject mTrackedObject;
+    private SteamVR_Controller.Device mController
+    {
+       get
+        {
+            return SteamVR_Controller.Input((int)mTrackedObject.index);
+        }
+    }
+
+    // Wake up the controllers
+    void Awake()
+    {
+        mTrackedObject = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // 1
+        if (mController.GetAxis() != Vector2.zero)
+        {
+            Debug.Log(gameObject.name + mController.GetAxis());
+        }
+
+        // 2
+        if (mController.GetHairTriggerDown())
+        {
+            Debug.Log(gameObject.name + " Trigger Press");
+        }
+
+        // 3
+        if (mController.GetHairTriggerUp())
+        {
+            Debug.Log(gameObject.name + " Trigger Release");
+        }
+
+        // 4
+        if (mController.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            Debug.Log(gameObject.name + " Grip Press");
+        }
+
+        // 5
+        if (mController.GetPressUp(SteamVR_Controller.ButtonMask.Grip))
+        {
+            Debug.Log(gameObject.name + " Grip Release");
+        }
+    }
+}
